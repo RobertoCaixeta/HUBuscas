6512f1ff26735238b7ca3cfd6f3442b82cf4c8f5
robertocaixeta <robertocaixeta102@gmail.com>
2022-01-06 13:53:43 -0300

    :star: Adiciona component de historico

--- hubuscas/src/App.js
+++ hubuscas/src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react'
 import './App.css';
 import MainPage from './pages/MainPage';
 
 
+
 function App() {
+
   return (
-    <MainPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- hubuscas/src/pages/MainPage/index.js
+++ hubuscas/src/pages/MainPage/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../services/api'
-import { Background, Container, Input, Button, Img, ImgContainer, Text } from './styles';
+import { Background, Container, Input, Button, Img, ImgContainer, Text, UserContainer, User } from './styles';
 import Modal from 'react-modal';
 import Infos from '../Infos';
 
@@ -23,6 +23,8 @@ export default function MainPage() {
     const [search, setSearch] = useState("");
     const [input, setInput] = useState("")
     const [modalIsOpen, setIsOpen] = useState(false);
+    const [historicoIsOpen, setHistoricoIsOpen] = useState(false);
+    const [searchedUsers, setSearchedUsers] = useState([]);
 
     function handleSubmit() {
         setSearch(input)
@@ -33,13 +35,22 @@ export default function MainPage() {
     }
 
 
-
     function openModal() {
+        setHistoricoIsOpen(false);
         setIsOpen(true);
+        
     }
 
     function closeModal() {
         setIsOpen(false);
+    }
+
+    function openHistorico() {
+        setHistoricoIsOpen(true);
+    }
+
+    function closeHistorico() {
+        setHistoricoIsOpen(false);
     }
 
     useEffect(() => {
@@ -47,6 +58,11 @@ export default function MainPage() {
             setUser(data)
         })
     }, [search]);
+
+    useEffect(() => {
+        setSearchedUsers([...searchedUsers, user])
+
+    }, [user]);
 
     return (
         <Background>
@@ -67,13 +83,37 @@ export default function MainPage() {
                     <Button onClick={handleSubmit} > Pesquisar usuário</Button>
                 </div>
                 <ImgContainer>
-                    <Img onClick={openModal} src={user.avatar_url} />
+                    <Img onClick={openModal} src={user.avatar_url} size={"400px"}/>
                 </ImgContainer>
                 <Text> Nome: {user.name}</Text>
                 <Text> Login: {user.login}</Text>
                 <Text> Localização: {user.location}</Text>
+                <Button onClick={openHistorico} > Histórico de Pesquisa</Button>
+                <Modal
+                    isOpen={historicoIsOpen}
+                    onRequestClose={closeHistorico}
+                    style={customStyles}
+                    ariaHideApp={false}
+                >
+                    <UserContainer>
+                        {searchedUsers.map(searchedUser => (
+                            <User key={searchedUser?.id}>
+                                <Img onClick={openModal} src={searchedUser.avatar_url} size={"100px"}/>
+                                <Text >Nome: {searchedUser.name}</Text>
+                                <Text >Login: {searchedUser.login}</Text>
+                                <Text >Localização: {searchedUser.location}</Text>
+                            </User>
+
+                        ))}
+                    </UserContainer>
+
+
+                    <Button onClick={closeHistorico}>Fechar HIstórico</Button>
+
+                </Modal>
 
             </Container>
+
         </Background>
     )
 }
